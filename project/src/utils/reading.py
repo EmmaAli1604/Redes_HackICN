bb9b77305b26e7c59a7afec0905715d981ce14c1
MarcoAntonioConcha <marcoantonioconcha@ciencias.unam.mx>
2025-10-25 15:38:31 -0600

    path update at reading.py

--- project/src/utils/reading.py
+++ project/src/utils/reading.py
@@ -1,52 +1,60 @@
 import pandas as pd
 
+from pathlib import Path
+import pandas as pd
+
+# Directorio del archivo actual
+BASE_DIR = Path(__file__).parent
+
 # Branches
-branches_20240521 = pd.read_csv("../../datasets/branches_2024-05-21.csv")
-branches_20240704 = pd.read_csv("../../datasets/branches_2024-07-04.csv")
-branches_20240911 = pd.read_csv("../../datasets/branches_2024-09-11.csv")
-branches_20241224 = pd.read_csv("../../datasets/branches_2024-12-24.csv")
-branches_20250131 = pd.read_csv("../../datasets/branches_2025-01-31.csv")
-branches_20250213 = pd.read_csv("../../datasets/branches_2025-02-13.csv")
-branches_20250416 = pd.read_csv("../../datasets/branches_2025-04-16.csv")
-branches_20250606 = pd.read_csv("../../datasets/branches_2025-06-06.csv")
-branches_20250825 = pd.read_csv("../../datasets/branches_2025-08-25.csv")
-branches_20251010 = pd.read_csv("../../datasets/branches_2025-10-10.csv")
+# Branches (dos niveles arriba si estás en src/utils/)
+branches_20240521 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2024-05-21.csv")
+branches_20240704 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2024-07-04.csv")
+branches_20240911 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2024-09-11.csv")
+branches_20241224 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2024-12-24.csv")
+branches_20250131 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2025-01-31.csv")
+branches_20250213 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2025-02-13.csv")
+branches_20250416 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2025-04-16.csv")
+branches_20250606 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2025-06-06.csv")
+branches_20250825 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2025-08-25.csv")
+branches_20251010 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "branches_2025-10-10.csv")
 
 # Generators
-generators_20240521 = pd.read_csv("../../datasets/generators_2024-05-21.csv")
-generators_20240704 = pd.read_csv("../../datasets/generators_2024-07-04.csv")
-generators_20240911 = pd.read_csv("../../datasets/generators_2024-09-11.csv")
-generators_20241224 = pd.read_csv("../../datasets/generators_2024-12-24.csv")
-generators_20250131 = pd.read_csv("../../datasets/generators_2025-01-31.csv")
-generators_20250213 = pd.read_csv("../../datasets/generators_2025-02-13.csv")
-generators_20250416 = pd.read_csv("../../datasets/generators_2025-04-16.csv")
-generators_20250606 = pd.read_csv("../../datasets/generators_2025-06-06.csv")
-generators_20250825 = pd.read_csv("../../datasets/generators_2025-08-25.csv")
-generators_20251010 = pd.read_csv("../../datasets/generators_2025-10-10.csv")
+generators_20240521 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2024-05-21.csv")
+generators_20240704 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2024-07-04.csv")
+generators_20240911 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2024-09-11.csv")
+generators_20241224 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2024-12-24.csv")
+generators_20250131 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2025-01-31.csv")
+generators_20250213 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2025-02-13.csv")
+generators_20250416 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2025-04-16.csv")
+generators_20250606 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2025-06-06.csv")
+generators_20250825 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2025-08-25.csv")
+generators_20251010 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "generators_2025-10-10.csv")
 
 # Loads
-loads_20240521 = pd.read_csv("../../datasets/loads_2024-05-21.csv")
-loads_20240704 = pd.read_csv("../../datasets/loads_2024-07-04.csv")
-loads_20240911 = pd.read_csv("../../datasets/loads_2024-09-11.csv")
-loads_20241224 = pd.read_csv("../../datasets/loads_2024-12-24.csv")
-loads_20250131 = pd.read_csv("../../datasets/loads_2025-01-31.csv")
-loads_20250213 = pd.read_csv("../../datasets/loads_2025-02-13.csv")
-loads_20250416 = pd.read_csv("../../datasets/loads_2025-04-16.csv")
-loads_20250606 = pd.read_csv("../../datasets/loads_2025-06-06.csv")
-loads_20250825 = pd.read_csv("../../datasets/loads_2025-08-25.csv")
-loads_20251010 = pd.read_csv("../../datasets/loads_2025-10-10.csv")
+loads_20240521 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2024-05-21.csv")
+loads_20240704 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2024-07-04.csv")
+loads_20240911 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2024-09-11.csv")
+loads_20241224 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2024-12-24.csv")
+loads_20250131 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2025-01-31.csv")
+loads_20250213 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2025-02-13.csv")
+loads_20250416 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2025-04-16.csv")
+loads_20250606 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2025-06-06.csv")
+loads_20250825 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2025-08-25.csv")
+loads_20251010 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "loads_2025-10-10.csv")
 
 # Monitored Lines
-monitored_lines_20240521 = pd.read_csv("../../datasets/monitored_lines_2024-05-21.csv")
-monitored_lines_20240704 = pd.read_csv("../../datasets/monitored_lines_2024-07-04.csv")
-monitored_lines_20240911 = pd.read_csv("../../datasets/monitored_lines_2024-09-11.csv")
-monitored_lines_20241224 = pd.read_csv("../../datasets/monitored_lines_2024-12-24.csv")
-monitored_lines_20250131 = pd.read_csv("../../datasets/monitored_lines_2025-01-31.csv")
-monitored_lines_20250213 = pd.read_csv("../../datasets/monitored_lines_2025-02-13.csv")
-monitored_lines_20250416 = pd.read_csv("../../datasets/monitored_lines_2025-04-16.csv")
-monitored_lines_20250606 = pd.read_csv("../../datasets/monitored_lines_2025-06-06.csv")
-monitored_lines_20250825 = pd.read_csv("../../datasets/monitored_lines_2025-08-25.csv")
-monitored_lines_20251010 = pd.read_csv("../../datasets/monitored_lines_2025-10-10.csv")
+monitored_lines_20240521 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2024-05-21.csv")
+monitored_lines_20240704 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2024-07-04.csv")
+monitored_lines_20240911 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2024-09-11.csv")
+monitored_lines_20241224 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2024-12-24.csv")
+monitored_lines_20250131 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2025-01-31.csv")
+monitored_lines_20250213 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2025-02-13.csv")
+monitored_lines_20250416 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2025-04-16.csv")
+monitored_lines_20250606 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2025-06-06.csv")
+monitored_lines_20250825 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2025-08-25.csv")
+monitored_lines_20251010 = pd.read_csv(BASE_DIR / ".." / ".." / "datasets" / "monitored_lines_2025-10-10.csv")
+
 
 branches_20240521['monitored'] = branches_20240521['branch_name'].isin(
     monitored_lines_20240521['branch_name']
@@ -119,5 +127,16 @@ def get_day_20250825():
 def get_day_20251010():
     return branches_20251010, generators_20251010, loads_20251010
 
-print(branches_20251010.info())
-print(monitored_lines_20240521.info())
+
+
+print("Branches")
+print(branches_20251010.head())
+
+print("generadores")
+print(generators_20251010.head())
+
+print("loads")
+print(loads_20251010.head())
+
+dataframe = get_day_20240521()
+print(dataframe)
