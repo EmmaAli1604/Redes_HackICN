8ca847af0025b404e82ce8f995712c5931aed63e
Alejandro Jacome <jacomedelgadoa@ciencias.unam.mx>
2025-10-26 00:04:09 +0000

    agragado SA.py

---
+++ project/src/SA.py
@@ -0,0 +1,50 @@
+class SA:
+    def __init__(self, temp, cooling_rate, current_solution, best_solution, size, rnd, e, lim):
+
+        self.initial_temperature = temp
+        self.cooling_rate = cooling_rate
+        self.best_solution = best_solution
+        self.size_lote = size
+        self.random = rnd
+        self.e = e 
+        self.limit = lim
+
+    def calculate_lote(self):
+        cost = 0.0
+        c = 0
+        i = 0
+        while usize <= self.size_lote:
+            if i == self.size_lote * self.limit:
+                break
+            
+            vecino = self.current_solution.neighbour()
+            
+            if vecino[1] self.current_solution.get_cost() + self.initial_temperature:
+                self.currenti_solution.update(vecino);
+                c += 1
+                cost += self.current_solution.get_cost()
+
+                if self.current_solution.get_cost() < self.best_solution.get_cost():
+                    self.best_solution = self.current_solution.clone()
+            i+=1
+        return (cost/self.size_lote,i)
+    
+    def accepot_threshold(self):
+        p = 0
+        total = 0
+        while self.initial_temperature > self.e:
+            q = float('inf')
+            while p <= q:
+                q = p
+                new_p = self.calculate_lote();
+                p = new_p
+                total += i
+                if i == self.size_lote * self.limit:
+                    return total
+            self.initial_temperature *= self.cooling_rate
+        return total
+
+    def get_best_solution(self):
+        self.best_solution.get_min()
+        return.best_solution
+                
